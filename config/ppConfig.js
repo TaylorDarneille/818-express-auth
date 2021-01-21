d7a1ce3e8620d17e996305f0cce9c10b480245cf
TaylorDarneille <tmdarneille@dons.usfca.edu>
2021-01-21 07:11:01 -0800

    added better comments to passport config file

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -9,6 +9,8 @@ passport.serializeUser((user, doneCallback)=>{
     doneCallback(null, user.id)
 })
 
+// tells passport how to convert the serialized user (the id)
+// back to the original user object
 passport.deserializeUser((id, doneCallback)=>{
     db.user.findByPk(id)
     .then(foundUser=>{
@@ -20,24 +22,7 @@ passport.deserializeUser((id, doneCallback)=>{
     })
 })
 
-/*
- This is Passport's strategy to provide local authentication. We provide the
- following information to the LocalStrategy:
-
- Configuration: An object of data to identify our authentication fields, the
- username and password
-
- Callback function: A function that's called to log the user in. We can pass
- the email and password to a database query, and return the appropriate
- information in the callback. Think of "doneCallback" as a function that'll later look
- like this:
- login(error, user) {
-  // do stuff
- }
- We need to provide the error as the first argument, and the user as the
- second argument. We can provide "null" if there's no error, or "false" if
- there's no user.
-*/
+// ---------------------> STRATEGY SET UP <------------------------
 
 const findAndLogInUser = (email, password, doneCallback) =>{
     db.user.findOne({where:{email: email}}) // go check for a user in the db with that email
@@ -56,18 +41,29 @@ const findAndLogInUser = (email, password, doneCallback) =>{
     .catch(err=>doneCallback(err)) // doneCallback takes two params: error, userToBeLoggedIn
 }
 
+/*
+Think of "doneCallback" as a function that looks like this:
+ login(error, userToBeLoggedIn) {
+  // do stuff
+ }
+We provide "null" if there's no error, or "false" if there's no user 
+(like they did in the docs).
+*/
+
 const fieldsToCheck = {
     usernameField: 'email', // tells passport that the email is what we're using as 'username'
     passwordField: 'password'
 }
 
-// Create an instance of Local Strategy
-// --> constructor arg 1:
-// an object that indicates how we're going refer to the two fields
-// we're checking (for ex. we're using email instead of username)
-// --> constructor arg 2:
-// a callback that is ready to receive the two fields we're checking
-// as well as a doneCallback
+/*
+Create an instance of Local Strategy
+--> constructor arg 1:
+an object that indicates how we're going refer to the two fields
+we're checking (for ex. we're using email instead of username)
+--> constructor arg 2:
+a callback that is ready to receive the two fields we're checking
+as well as a doneCallback
+*/
 const strategy = new LocalStrategy(fieldsToCheck, findAndLogInUser)
 
 // tell this instance of passport to use the strategy we just configured
